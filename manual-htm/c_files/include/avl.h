d270d30e9d88244e3bab333186c131c254e95618
BazookaMusic <sotirisdragonas@gmail.com>
2020-04-12 23:53:39 +0300

    avl impl

---
+++ manual-htm/c_files/include/avl.h
@@ -0,0 +1,21 @@
+#ifndef _AVL_T_
+#define _AVL_T_
+
+#define NODE_HAS_HEIGHT
+#define BST_INTERNAL
+#include "bst.h"
+
+typedef bst_node_t avl_node_t;
+typedef bst_t avl_t;
+
+#define MAX(a,b) ( (a) >= (b) ? (a) : (b) )
+
+//> Used as `data` for marked nodes (zombie nodes)
+#define MARKED_NODE ((void *)0xffffLLU)
+
+avl_t *avl_new()
+{
+	return _bst_new_helper();
+}
+
+#endif /* _AVL_T_ */
